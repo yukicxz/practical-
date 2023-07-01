76e17453e802a6e85fb07547167d3d78b93b362f
yukicxz <btw.yuki@hotmail.com>
2023-07-01 16:52:37 +0300

    interface.v1

--- all.h
+++ all.h
@@ -1,1 +1,14 @@
-#pragma once
+#ifndef all_h
+#define all_h
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+#include <locale.h>
+#include <string.h>
+#include <windows.h>
+
+int show_menu();
+void func1();
+void func2();
+#endif // !all_h
