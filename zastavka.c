0dd9bae2e8bfef6ec985988bc57e42975092b395
yukicxz <btw.yuki@hotmail.com>
2023-07-03 12:45:05 +0300

    Добавил комментарии

--- zastavka.c
+++ zastavka.c
@@ -12,44 +12,44 @@ int zastavka()
     int length3 = strlen(title3); //
     int x, y;
 
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    int console_width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-    int console_height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    CONSOLE_SCREEN_BUFFER_INFO csbi; //
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi); //
+    int console_width = csbi.srWindow.Right - csbi.srWindow.Left + 1; // Установка размеров консоли
+    int console_height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1; //
 
-    x = (console_width - length) / 2;
-    y = console_height / 2;
+    x = (console_width - length) / 2; // Определение центра консоли
+    y = console_height / 2; //
 
 
-    for (int i = 0; i < y; i++)
-        printf("\n");
-    for (int i = 0; i < x; i++)
-        printf(" ");
-    for (int j = 0; j < length; j++)
+    for (int i = 0; i < y; i++) //добавление отступов до центра по y
+        printf("\n"); //
+    for (int i = 0; i < x; i++) //добавление отступов до центра по x
+        printf(" "); //
+    for (int j = 0; j < length; j++) 
     {
-        printf("\033[91m""%c""\033[m", title[j]);
-        Sleep(100);
+        printf("\033[91m""%c""\033[m", title[j]); //посимвольный вывод 1 массива
+        Sleep(60); //промежуток вывода каждого символа
     }
-    Sleep(1000);
+    Sleep(500);
 
-    for (int i = 0; i < 35; i++)
+    for (int i = 0; i < 35; i++) //добавление отступов по x
         printf(" ");
-    for (int m = 0; m < length2; m++)
+    for (int m = 0; m < length2; m++) 
     {
-        printf("%c", title2[m]);
-        Sleep(100);
+        printf("%c", title2[m]);//посимвольный вывод 2 массива
+        Sleep(60); //промежуток вывода каждого символа
     }
-    Sleep(1000);
-    for (int l = 0; l < y; l++)
+    Sleep(500);
+    for (int l = 0; l < y; l++) //добавление отступов по y
         printf("\n");
-    for (int l = 0; l < x; l++)
+    for (int l = 0; l < x; l++) //добавление отступов по x
         printf(" ");
     for (int n = 0; n < length3; n++)
     {
-        printf("%c", title3[n]);
-        Sleep(100);
+        printf("%c", title3[n]); //посимвольный вывод 3 массива
+        Sleep(60); //промежуток вывода каждого символа
     }
     Sleep(2000);
-    system("cls");
+    system("cls"); //очистка консоли
     return 0;
 }
